7680a91093bb4720f3c438efa27ff53fc652601d
Carlo Wood <carlo@alinoe.com>
1999-04-06 01:42:25 +0000

    Initial import

---
+++ which/bash.c
@@ -0,0 +1,480 @@
+/* Copyright (C) 1987 - 1999 Free Software Foundation, Inc.
+
+   This file is based on stuff from GNU Bash 1.14.7, the Bourne Again SHell.
+   Everything that was changed is marked with the word `CHANGED'.
+
+   Which is free software; you can redistribute it and/or modify it
+   under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 1, or (at your option)
+   any later version.
+
+   Which is distributed in the hope that it will be useful, but WITHOUT
+   ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+   or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public
+   License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with Bash; see the file COPYING.  If not, write to the Free
+   Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA. */
+
+#include <sys/stat.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <libiberty.h>
+#include <readline/tilde.h>
+#include "bash.h"
+
+extern int skip_tilde, skip_dot;
+
+/*
+ * CHANGED:
+ * No need to make this work on other OS than linux.
+ * The following macro's are used below:
+ */
+#define HAVE_GETGROUPS
+#define GID_T gid_t
+#undef SHELL
+#undef AFS
+#undef NOGROUP
+
+/*
+ * CHANGED:
+ * - Added prototypes,
+ * - used ANSI function arguments,
+ * - made all functions static and
+ * - changed all occurences of 'char *' into 'const char *' where possible.
+ */
+static int group_member (GID_T gid);
+static int file_status (const char *name);
+static int absolute_program (const char *string);
+static char *extract_colon_unit (const char *string, int *p_index);
+static char *get_next_path_element (const char *path_list, int *path_index_pointer);
+static int same_file (const char *path1, const char *path2, const struct stat *stp1, const struct stat *stp2);
+static char *make_full_pathname (const char *path, const char *name, int name_len);
+
+/*===========================================================================
+ *
+ * Almost everything below is from execute_cmd.c from bash-1.14.7,
+ * a few functions are from other files: test.c, general.c and variables.c.
+ *
+ */
+
+#if !defined (savestring)
+#  define savestring(x) (char *)strcpy (xmalloc (1 + strlen (x)), (x))
+#endif /* !savestring */
+
+#if defined (HAVE_GETGROUPS)
+/* The number of groups that this user is a member of. */
+static int ngroups = 0;
+static GID_T *group_array = (GID_T *)NULL;
+static int default_group_array_size = 0;
+#endif /* HAVE_GETGROUPS */
+
+#if !defined (NOGROUP)
+#  define NOGROUP (GID_T) -1
+#endif
+
+/* Return non-zero if GID is one that we have in our groups list. */
+static int
+group_member (GID_T gid)
+{
+  static GID_T pgid = (GID_T)NOGROUP;
+  static GID_T egid = (GID_T)NOGROUP;
+
+  if (pgid == (GID_T)NOGROUP)
+#if defined (SHELL)
+    pgid = (GID_T) current_user.gid;
+#else /* !SHELL */
+    pgid = (GID_T) getgid ();
+#endif /* !SHELL */
+
+  if (egid == (GID_T)NOGROUP)
+#if defined (SHELL)
+    egid = (GID_T) current_user.egid;
+#else /* !SHELL */
+    egid = (GID_T) getegid ();
+#endif /* !SHELL */
+
+  if (gid == pgid || gid == egid)
+    return (1);
+
+#if defined (HAVE_GETGROUPS)
+  /* getgroups () returns the number of elements that it was able to
+     place into the array.  We simply continue to call getgroups ()
+     until the number of elements placed into the array is smaller than
+     the physical size of the array. */
+
+  while (ngroups == default_group_array_size)
+    {
+      default_group_array_size += 64;
+
+      group_array = (GID_T *)
+        xrealloc (group_array, default_group_array_size * sizeof (GID_T));
+
+      ngroups = getgroups (default_group_array_size, group_array);
+    }
+
+  /* In case of error, the user loses. */
+  if (ngroups < 0)
+    return (0);
+
+  /* Search through the list looking for GID. */
+  {
+    register int i;
+
+    for (i = 0; i < ngroups; i++)
+      if (gid == group_array[i])
+        return (1);
+  }
+#endif /* HAVE_GETGROUPS */
+
+  return (0);
+}
+
+#define u_mode_bits(x) (((x) & 0000700) >> 6)
+#define g_mode_bits(x) (((x) & 0000070) >> 3)
+#define o_mode_bits(x) (((x) & 0000007) >> 0)
+#define X_BIT(x) ((x) & 1)
+
+/* Return some flags based on information about this file.
+   The EXISTS bit is non-zero if the file is found.
+   The EXECABLE bit is non-zero the file is executble.
+   Zero is returned if the file is not found. */
+static int
+file_status (const char *name)
+{
+  struct stat finfo;
+  static int user_id = -1;
+
+  /* Determine whether this file exists or not. */
+  if (stat (name, &finfo) < 0)
+    return (0);
+
+  /* If the file is a directory, then it is not "executable" in the
+     sense of the shell. */
+  if (S_ISDIR (finfo.st_mode))
+    return (FS_EXISTS);
+
+#if defined (AFS)
+  /* We have to use access(2) to determine access because AFS does not
+     support Unix file system semantics.  This may produce wrong
+     answers for non-AFS files when ruid != euid.  I hate AFS. */
+  if (access (name, X_OK) == 0)
+    return (FS_EXISTS | FS_EXECABLE);
+  else
+    return (FS_EXISTS);
+#else /* !AFS */
+
+  /* Find out if the file is actually executable.  By definition, the
+     only other criteria is that the file has an execute bit set that
+     we can use. */
+  if (user_id == -1)
+    user_id = geteuid (); /* CHANGED: bash uses: current_user.euid; */
+
+  /* Root only requires execute permission for any of owner, group or
+     others to be able to exec a file. */
+  if (user_id == 0)
+    {
+      int bits;
+
+      bits = (u_mode_bits (finfo.st_mode) |
+              g_mode_bits (finfo.st_mode) |
+              o_mode_bits (finfo.st_mode));
+
+      if (X_BIT (bits))
+        return (FS_EXISTS | FS_EXECABLE);
+    }
+
+  /* If we are the owner of the file, the owner execute bit applies. */
+  if (user_id == finfo.st_uid && X_BIT (u_mode_bits (finfo.st_mode)))
+    return (FS_EXISTS | FS_EXECABLE);
+
+  /* If we are in the owning group, the group permissions apply. */
+  if (group_member (finfo.st_gid) && X_BIT (g_mode_bits (finfo.st_mode)))
+    return (FS_EXISTS | FS_EXECABLE);
+
+  /* If `others' have execute permission to the file, then so do we,
+     since we are also `others'. */
+  if (X_BIT (o_mode_bits (finfo.st_mode)))
+    return (FS_EXISTS | FS_EXECABLE);
+  else
+    return (FS_EXISTS);
+#endif /* !AFS */
+}
+
+/* Return 1 if STRING is an absolute program name; it is absolute if it
+   contains any slashes.  This is used to decide whether or not to look
+   up through $PATH. */
+static int
+absolute_program (const char *string)
+{
+  return ((char *)strchr (string, '/') != (char *)NULL);
+}
+
+/* Given a string containing units of information separated by colons,
+   return the next one pointed to by (P_INDEX), or NULL if there are no more.
+   Advance (P_INDEX) to the character after the colon. */
+static char *
+extract_colon_unit (const char *string, int *p_index)
+{
+  int i, start;
+
+  i = *p_index;
+
+  if (!string || (i >= (int)strlen (string)))
+    return ((char *)NULL);
+
+  /* Each call to this routine leaves the index pointing at a colon if
+     there is more to the path.  If I is > 0, then increment past the
+     `:'.  If I is 0, then the path has a leading colon.  Trailing colons
+     are handled OK by the `else' part of the if statement; an empty
+     string is returned in that case. */
+  if (i && string[i] == ':')
+    i++;
+
+  start = i;
+
+  while (string[i] && string[i] != ':') i++;
+
+  *p_index = i;
+
+  if (i == start)
+    {
+      if (string[i])
+        (*p_index)++;
+
+      /* Return "" in the case of a trailing `:'. */
+      return (savestring (""));
+    }
+  else
+    {
+      char *value;
+
+      value = xmalloc (1 + i - start);
+      strncpy (value, string + start, i - start);
+      value [i - start] = '\0';
+
+      return (value);
+    }
+}
+
+/* Return the next element from PATH_LIST, a colon separated list of
+   paths.  PATH_INDEX_POINTER is the address of an index into PATH_LIST;
+   the index is modified by this function.
+   Return the next element of PATH_LIST or NULL if there are no more. */
+static char *
+get_next_path_element (const char *path_list, int *path_index_pointer)
+{
+  char *path;
+
+  path = extract_colon_unit (path_list, path_index_pointer);
+
+  if (!path)
+    return (path);
+
+  if (!*path)
+    {
+      free (path);
+      path = savestring (".");
+    }
+
+  return (path);
+}
+
+/* Return 1 if PATH1 and PATH2 are the same file.  This is kind of
+   expensive.  If non-NULL STP1 and STP2 point to stat structures
+   corresponding to PATH1 and PATH2, respectively. */
+static int
+same_file (
+     const char *path1,
+     const char *path2,
+     const struct stat *stp1,
+     const struct stat *stp2)
+{
+  struct stat st1, st2;
+
+  if (stp1 == NULL)
+    {
+      if (stat (path1, &st1) != 0)
+        return (0);
+      stp1 = &st1;
+    }
+
+  if (stp2 == NULL)
+    {
+      if (stat (path2, &st2) != 0)
+        return (0);
+      stp2 = &st2;
+    }
+
+  return ((stp1->st_dev == stp2->st_dev) && (stp1->st_ino == stp2->st_ino));
+}
+
+/* DOT_FOUND_IN_SEARCH becomes non-zero when find_user_command ()
+   encounters a `.' as the directory pathname while scanning the
+   list of possible pathnames; i.e., if `.' comes before the directory
+   containing the file of interest. */
+static int dot_found_in_search = 0;
+
+/* Turn PATH, a directory, and NAME, a filename, into a full pathname.
+   This allocates new memory and returns it. */
+static char *
+make_full_pathname (const char *path, const char *name, int name_len)
+{
+  char *full_path;
+  int path_len;
+
+  path_len = strlen (path);
+  full_path = (char *) xmalloc (2 + path_len + name_len);
+  strcpy (full_path, path);
+  full_path[path_len] = '/';
+  strcpy (full_path + path_len + 1, name);
+  return (full_path);
+}
+
+/* This does the dirty work for find_path_file () and find_user_command ().
+   NAME is the name of the file to search for.
+   PATH_LIST is a colon separated list of directories to search.
+   FLAGS contains bit fields which control the files which are eligible.
+   Some values are:
+      FS_EXEC_ONLY:             The file must be an executable to be found.
+      FS_EXEC_PREFERRED:        If we can't find an executable, then the
+                                the first file matching NAME will do.
+      FS_EXISTS:                The first file found will do.
+*/
+const char *
+find_user_command_in_path (const char *name, const char *path_list, int flags)
+{
+  char *full_path;
+  char *file_to_lose_on;
+  int status, path_index, name_len;
+  struct stat finfo;
+
+  name_len = strlen (name);
+
+  /* The file name which we would try to execute, except that it isn't
+     possible to execute it.  This is the first file that matches the
+     name that we are looking for while we are searching $PATH for a
+     suitable one to execute.  If we cannot find a suitable executable
+     file, then we use this one. */
+  file_to_lose_on = (char *)NULL;
+
+  /* We haven't started looking, so we certainly haven't seen
+     a `.' as the directory path yet. */
+  dot_found_in_search = 0;
+
+  if (absolute_program (name))
+    {
+      full_path = (char *) xmalloc (1 + name_len);
+      strcpy (full_path, name);
+
+      status = file_status (full_path);
+
+      /* If the file doesn't exist, quit now. */
+      if (!(status & FS_EXISTS))
+        {
+          free (full_path);
+          return ((char *)NULL);
+        }
+
+      /* If we only care about whether the file exists or not, return
+         this filename. */
+      if (flags & FS_EXISTS)
+        return (full_path);
+
+      /* Otherwise, maybe we care about whether this file is executable.
+         If it is, and that is what we want, return it. */
+      if ((flags & FS_EXEC_ONLY) && (status & FS_EXECABLE))
+        return (full_path);
+      else
+        {
+          free (full_path);
+          return ((char *)NULL);
+        }
+    }
+
+  /* Find out the location of the current working directory. */
+  stat (".", &finfo);
+
+  path_index = 0;
+  while (path_list && path_list[path_index])
+    {
+      /* Allow the user to interrupt out of a lengthy path search. */
+      /* CHANGED: commented out: QUIT;*/
+
+      char *path = get_next_path_element (path_list, &path_index);
+
+      if (!path)
+        break;
+
+      /* CHANGED: Skip everything in the PATH that doesn't start with a '/' */
+      if (skip_tilde && *path != '/')
+      {
+        free (path);
+	continue;
+      }
+
+      if (*path == '~')
+        {
+          char *t = tilde_expand (path);
+          free (path);
+          path = t;
+        }
+
+      /* CHANGED: Skip everything in the PATH that doesn't start with a '/' or '~' */
+      if (skip_dot && *path != '/')
+      {
+        free (path);
+	continue;
+      }
+
+      /* Remember the location of "." in the path, in all its forms
+         (as long as they begin with a `.', e.g. `./.') */
+      if (!dot_found_in_search && (*path == '.') &&
+          same_file (".", path, &finfo, (struct stat *)NULL))
+        dot_found_in_search = 1;
+
+      full_path = make_full_pathname (path, name, name_len);
+      free (path);
+
+      status = file_status (full_path);
+
+      if (!(status & FS_EXISTS))
+        goto next_file;
+
+      /* The file exists.  If the caller simply wants the first file,
+         here it is. */
+      if (flags & FS_EXISTS)
+        return (full_path);
+
+       /* If the file is executable, then it satisfies the cases of
+          EXEC_ONLY and EXEC_PREFERRED.  Return this file unconditionally. */
+      if (status & FS_EXECABLE)
+        {
+          if (file_to_lose_on)
+	    free(file_to_lose_on);
+
+          return (full_path);
+        }
+
+      /* The file is not executable, but it does exist.  If we prefer
+         an executable, then remember this one if it is the first one
+         we have found. */
+      if (flags & FS_EXEC_PREFERRED)
+        {
+          if (!file_to_lose_on)
+            file_to_lose_on = savestring (full_path);
+        }
+
+    next_file:
+      free (full_path);
+    }
+
+  /* We didn't find exactly what the user was looking for.  Return
+     the contents of FILE_TO_LOSE_ON which is NULL when the search
+     required an executable, or non-NULL if a file was found and the
+     search would accept a non-executable as a last resort. */
+  return (file_to_lose_on);
+}
+
