d01a013481a7c0eea0f308130a6223ab1728f3fa
Carlo Wood <carlo@alinoe.com>
1999-04-09 12:50:53 +0000

    Moved find_user_command_in_path() from bash.c to which.c because I intend to heavily change it.

--- which/bash.h
+++ which/bash.h
@@ -3,4 +3,10 @@
 #define FS_EXEC_PREFERRED	0x4
 #define FS_EXEC_ONLY		0x8
 
-extern const char *find_user_command_in_path (const char *name, const char *path_list, int flags);
+#define savestring(x) (char *)strcpy (xmalloc (1 + strlen (x)), (x))
+
+extern int file_status (const char *name);
+extern int absolute_program (const char *string);
+extern char *get_next_path_element (const char *path_list, int *path_index_pointer);
+extern int same_file (const char *path1, const char *path2, const struct stat *stp1, const struct stat *stp2);
+extern char *make_full_pathname (const char *path, const char *name, int name_len);
