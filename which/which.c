0368a619fc8ce070289ed61318e64b15a5f6ab08
Carlo Wood <carlo@alinoe.com>
1999-04-09 23:36:33 +0000

    Bumped license to GPL version 2. Renamed the file COPYING to LICENSE.GPL.

--- which/which.c
+++ which/which.c
@@ -1,11 +1,11 @@
 /*
- * which v2.0 -- print full path of executables
+ * which v2.1 -- print full path of executables
  * Copyright (C) 1999  Carlo Wood <carlo@runaway.xs4all.nl>
  *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 1, or (at your option)
- * any later version.
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -14,7 +14,7 @@
  *
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
- * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  */
 
 #include <stdio.h>
@@ -40,7 +40,10 @@ static void print_usage(const char *progname)
 
 static void print_version(void)
 {
-  fprintf(stdout, "GNU which v2.1 -- by Carlo Wood\n");
+  fprintf(stdout, "GNU which v2.1, Copyright (C) 1999 Carlo Wood.\n");
+  fprintf(stdout, "GNU which comes with ABSOLUTELY NO WARRANTY;\n");
+  fprintf(stdout, "This program is free software; your freedom to use, change\n");
+  fprintf(stdout, "and distribute this program is covered by the GPL.\n");
 }
 
 static void print_fail(const char *progname, const char *name, const char *path_list)
