d5b39356ac0c02eb6eead07098546d54691942e4
Carlo Wood <carlo@alinoe.com>
2003-09-12 01:51:09 +0000

    Use 'function' keyword to declare shell function.

--- which/rpm/profile.d/which-2.sh
+++ which/rpm/profile.d/which-2.sh
@@ -1,5 +1,5 @@
 # Initialization script for bash and sh
-which() {
+function which() {
   (alias && declare -f) | /usr/bin/which --tty-only --read-alias --read-functions --show-dot --show-tilde $@
 }
 export -f which
