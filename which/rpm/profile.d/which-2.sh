d1fa5152f46b57639bf3e14cd39342bf36ec2031
cvs2git <>
2008-03-11 05:43:27 +0000

    This commit was manufactured by cvs2git to create tag 'V2_20'.
    Sprout from master 2008-03-11 05:43:26 UTC Carlo Wood <carlo@alinoe.com> '* tilde/xmalloc.h, removed unnecessary include of readline/rlstdc.h.'
    Delete:
        which/rpm/BUILD/.cvsignore
        which/rpm/RPMS/i386/.cvsignore
        which/rpm/SOURCES/.cvsignore
        which/rpm/SRPMS/.cvsignore
        which/rpm/macros
        which/rpm/profile.d/which-2.csh
        which/rpm/profile.d/which-2.sh
        which/rpm/rpmrc

--- which/rpm/profile.d/which-2.sh
+++
@@ -1,5 +0,0 @@
-# Initialization script for bash and sh
-function which() {
-  (alias && declare -f) | /usr/bin/which --tty-only --read-alias --read-functions --show-dot --show-tilde $@
-}
-export -f which
